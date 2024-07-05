af229f0d97bc1769b94bae78be5df3ab12987e15
OscarRapale <you@example.com>
2024-07-05 12:57:30 -0400

    Inserting data

--- db_schema.sql
+++ db_schema.sql
@@ -9,7 +9,9 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
+INSERT INTO users (id, email, password_hash, first_name, last_name, is_admin)
+VALUES ('4e7e4f43-0132-43d7-bfef-518e9ed70310','testing@example.com', 'password123', 'Testing', 'User', False),
+VALUES ('245300d5-479e-4fa9-a44b-fbaad0420f76','testing2@example.com', 'password456', 'Tester', 'Users', False);
 
 -- Create countries table
 CREATE TABLE IF NOT EXISTS countries (
@@ -18,7 +20,8 @@ CREATE TABLE IF NOT EXISTS countries (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
+INSERT INTO countries (code, name)
+VALUES ('PR', 'Puerto Rico');
 
 -- Create cities table
 CREATE TABLE IF NOT EXISTS cities (
@@ -29,7 +32,8 @@ CREATE TABLE IF NOT EXISTS cities (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (country_code) REFERENCES countries(code)
 );
-
+INSERT INTO cities (id, name, country_code)
+VALUES ('e552392a-6b5f-4df5-bc26-70021887d1ab','San Juan', 'PR')
 
 -- Create Places table
 CREATE TABLE IF NOT EXISTS places (
@@ -50,7 +54,8 @@ CREATE TABLE IF NOT EXISTS places (
     FOREIGN KEY (host_id) REFERENCES users(id),
     FOREIGN KEY (city_id) REFERENCES cities(id)
 );
-
+INSERT INTO places (id, name, description, address, city_id, latitude, longitude, host_id, number_of_rooms, number_of_bathrooms, price_per_night, max_guests)
+VALUES ('fe82bc9a-7b82-4798-83c1-cf0319a231c9','New Place', 'New place in the city', '123 Main Street', 'e552392a-6b5f-4df5-bc26-70021887d1ab', 40.7128, -74.006, '4e7e4f43-0132-43d7-bfef-518e9ed70310', 2, 1, 100.0, 2);
 
 -- Create Amenities table
 CREATE TABLE IF NOT EXISTS amenities (
@@ -59,7 +64,8 @@ CREATE TABLE IF NOT EXISTS amenities (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
+INSERT INTO amenities(id, name)
+VALUES ('d76727dd-0883-4084-8f74-f11f385cd2d5','Wi-Fi')
 
 -- Create Place_Amenities table (for many-to-many relationship)
 CREATE TABLE IF NOT EXISTS place_amenities (
@@ -69,7 +75,8 @@ CREATE TABLE IF NOT EXISTS place_amenities (
     FOREIGN KEY (place_id) REFERENCES places(id),
     FOREIGN KEY (amenity_id) REFERENCES amenities(id)
 );
-
+INSERT INTO place_amenities (place_id, amenity_id)
+VALUES ('', '')
 
 -- Create Reviews table
 CREATE TABLE IF NOT EXISTS reviews (
@@ -83,3 +90,5 @@ CREATE TABLE IF NOT EXISTS reviews (
     FOREIGN KEY (place_id) REFERENCES places(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+INSERT INTO reviews (id, place_id, user_id, rating, comment)
+VALUES ('0093925b-5c21-4b91-ae41-67eb0b311869', 'fe82bc9a-7b82-4798-83c1-cf0319a231c9', '245300d5-479e-4fa9-a44b-fbaad0420f76', 5, "Very nice place!")
